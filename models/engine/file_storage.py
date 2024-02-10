b3be66131c830022d73157b9744074b41c87121c
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-02-10 20:20:18 +0100

    create save and reload method ofFileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,63 @@
+#!/usr/bin/python3
+"""
+Thie module contains FileStorage class
+"""
 import json
+
 
 class FileStorage:
     """
-    This class is responsible for serializing instances to a JSON file and deserializing JSON files back into instances.
+    This class is responsible for serializing instances to a JSON fil
+    and deserializing JSON files back into instances.
     """
 
     __file_path = "file.json"
     __objects = {}
 
+    @property
+    def file_path(self):
+        return self.__file_path
+
+    @property
+    def objects(self):
+        return self.__objects
+
     def all(self):
+        """
+        returns the dictionary __objects
+        """
         return self.__objects
 
     def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        object_serialised = {}
-        for key, value in self.__objects.items():
-            object_serialised[key] = value.to_dict()
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        from models.base_model import BaseModel
+
+
+        def serialize(obj):
+            if isinstance(obj, BaseModel):
+                return obj.to_dict()
+            return obj
+
         with open(self.__file_path, "w") as file:
-            json.dump(object_serialised, file)
+            json.dump(self.__objects, file, default=serialize)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists;
+        otherwise, do nothing
+        """
+        import os
+
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r") as file:
+                self.__objects = json.load(file)
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,7 @@
-from models.engine.file_storage import FileStorage
 import unittest
+import os
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
@@ -7,4 +9,46 @@ class TestFileStorage(unittest.TestCase):
     Test cases for FileStorage class
     """
 
+    def setUp(self):
+        """Set up method"""
+        self.test_file_path = "file.json"
+
+        self.storage = FileStorage()
+
+
+    def tearDown(self):
+        """Teardown method"""
+
+        if os.path.exists(self.test_file_path):
+            os.remove(self.test_file_path)
+
+    def test_is_instance_of_FileStorage(self):
+        """test if storage is instance of FileStorage"""
+
+        self.assertIsInstance(self.storage, FileStorage)
+
+    def test_initializatuon(self):
+        """Test if storage is properly initialized"""
+
+        self.assertEqual(self.storage.file_path, self.test_file_path)
+        self.assertEqual(self.storage.objects, {})
+        self.assertIsInstance(self.storage.objects, dict)
+
+    def test_all_method(self):
+        """Test for all method"""
+        pass
+
+    def test_new_method(self):
+        """Test for new method"""
+        pass
+
+    def test_save_method(self):
+        """Test for save method"""
+        pass
     
+    def test_reload_method(self):
+        """Test for reload method"""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
