896c956b3a576f2b25fe9b5af906d45c9c329d5a
GODSPE1 <godspy18@gmail.com>
2024-02-11 19:35:58 -0800

    update the updste method

--- console.py
+++ console.py
@@ -3,11 +3,11 @@
 import cmd
 from models.base_model import BaseModel
 
+
 class HBNBCommand(cmd.Cmd):
     """This is all about the functions in the HBNB command"""
 
     prompt = "(hbnb) "
-
 
     def do_create(self, line):
         """
@@ -25,7 +25,6 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
         else:
             print("** class name missing **")
-
 
     def do_show(self, line):
         """
@@ -48,8 +47,6 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** no instance found **")
 
-
-
     def do_destroy(self, line):
         """
         Deletes an instance based on the class name and id.
@@ -70,7 +67,8 @@ class HBNBCommand(cmd.Cmd):
                 if instance_key in objects:
                     del objects[instance_key]
                     storage.save()
-                    if not any(instance.startswith(class_name + '.') for instance in objects.keys()):
+                    if not any(instance.startswith(class_name + '.')
+                    for instance in objects.keys()):
                         del storage.classes()[class_name.lower()]
                 else:
                     print("** no instance found **")
@@ -88,10 +86,9 @@ class HBNBCommand(cmd.Cmd):
             except NameError:
                 print("** class doesn't exist **")
                 return
-                print([str(obj) for obj in storage.all().values() if isinstance(obj, cls)])
+            print([str(obj) for obj in storage.all().values() if isinstance(obj, cls)])
         else:
             print([str(obj) for obj in storage.all().values()])
-
 
     def do_update(self, line):
         args = line.split()
@@ -103,21 +100,39 @@ class HBNBCommand(cmd.Cmd):
             print("** value missing **")
         elif len(args) == 3:
             class_name, attribut_name, attribut_value = args
-
+            objects = storage.all()
+        instance_key = "{}.{}".format(class_name, instance_id)
+        if class_name not in storage.classes():
+            print("** class doesn't exist **")
+        elif instance_key not in objects:
+            print("** no instance found **")
+        else:
+            obj = objects[instance_key]
+            if attribute_name in ["id", "created_at", "updated_at"]:
+                print("** cannot update id, created_at, or updated_at **")
+            else:
+                attr_type = type(getattr(obj, attribute_name))
+                try:
+                    casted_value = attr_type(attribute_value)
+                except ValueError:
+                    return
+                setattr(obj, attribute_name, casted_value)
+                obj.save()
 
     def do_EOF(self, line):
         """Handles end of the file to exit the program"""
         exit(0)
-    
+
     def emptyline(self):
         """Overides the cmd emptyline"""
-   
+
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
-    
+
     def default(self, line):
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
